55778a1c5c964c522048fbe8f3556e74881dae03
jesus is king <fadi.b.alhabib@gmail.com>
2025-02-07 21:52:21 +0200

    wheel game

--- src/components/modals/WinModal.tsx
+++ src/components/modals/WinModal.tsx
@@ -1,4 +1,6 @@
 import {
+  Center,
+  Heading,
   Image,
   Modal,
   ModalBody,
@@ -10,6 +12,7 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import closeIcon from "../../assets/basics/close-icon.png";
+import winAnimation from "../../assets/basics/win-animation.gif";
 interface WinModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -19,33 +22,53 @@ interface WinModalProps {
 
 function WinModal({ isOpen, onClose, stockName, currentPrice }: WinModalProps) {
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="md">
+    <Modal isOpen={isOpen} onClose={onClose} size="md" isCentered>
       <ModalOverlay />
-      <ModalContent borderRadius={"25px"} background={"black"}>
+      <ModalContent
+        borderRadius={"12vh"}
+        border={"2vh solid #1ED760"}
+        background={"rgba(0,0,0,0.85)"}
+      >
         <ModalHeader
           textTransform="lowercase"
           fontSize="2xl"
           fontWeight="medium"
           pb={2}
         >
-          <Image src={closeIcon} onClick={onClose} cursor={"pointer"}></Image>
+          <Image
+            src={closeIcon}
+            onClick={onClose}
+            cursor={"pointer"}
+            width={"4vh"}
+            height={"4vh"}
+          />
         </ModalHeader>
 
-        <ModalBody pb={6}>
+        <ModalBody pb={6} textColor={"white"}>
           <VStack spacing={3} align="stretch">
-            <Text fontSize="lg">
-              Stock name:{" "}
-              <Text as="span" fontWeight="semibold">
-                {stockName}
-              </Text>
-            </Text>
+            <Center>
+              <Image src={winAnimation} width={"20vh"} />
+            </Center>
+            <VStack>
+              <Heading>congratulations!</Heading>
 
-            <Text fontSize="lg">
-              Current Price:{" "}
-              <Text as="span" fontWeight="bold">
-                ${currentPrice}
+              <Text fontSize={"xl"} fontWeight="semibold">
+                You won
+                <Text display={"inline"} fontWeight={"bold"} color={"#1ED760"}>
+                  {" "}
+                  {stockName}{" "}
+                </Text>
+                Stock!
               </Text>
-            </Text>
+
+              <Text fontSize="lg">
+                Current Price:
+                <Text as="span" fontWeight="bold">
+                  ${currentPrice}
+                </Text>
+              </Text>
+            </VStack>
+
             {/* 
             <Text fontSize="lg" color={greenColor} fontWeight="medium">
               +3.2%
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+// Defaults to 400 weight
+// Bold
+
 import App from "./App.tsx";
 
 import SpinWheel from "./spinTheWheel/SpinWheel.tsx";
